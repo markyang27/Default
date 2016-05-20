1e4bbcc090c5268187a76d6d96477f2efd87458f
MY <markyang@vipabc.com>
2016-05-20 21:14:06 +0800

    init

---
+++ D3/D3/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(D3.Startup))]
+namespace D3
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
